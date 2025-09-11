8cdb53bed2cbc77e1ddd87db79668e1d02b04ba8
fabianfrank-ai <frankfabian945@outlook.de>
2025-09-11 13:38:15 +0300

    Initial commit

---
+++ Hello World.py
@@ -0,0 +1,4 @@
+#define what text you want to display
+text= "Hello World!"
+#print the desired text
+print(text)
---
+++ exercise1_5.py
@@ -0,0 +1,2 @@
+print("First name\tYear of Birth\tSalary\tTax Percentage")
+print("\tMatt\t1970\t2000\t22.8")
