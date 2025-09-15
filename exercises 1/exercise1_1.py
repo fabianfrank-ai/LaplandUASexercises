01e4450556147d5610e8eb5d24b9b5f762a0c97f
Fabian Frank <frankfabian945@outlook.de>
2025-09-15 13:55:41 +0300

    Update exercise1_1.py

--- exercises 1/exercise1_1.py
+++ exercises 1/exercise1_1.py
@@ -1,8 +1,8 @@
 #Create variables with data of desired person
-text="Hello World!"
-name="Person Name"
-birthyear=1975
-address="123 Road B1, 12345 City"
+text = "Hello World!"
+name = "Person Name"
+birthyear = 1975
+address = "123 Road B1, 12345 City"
 
 
 #display all the variables(data) line by line
