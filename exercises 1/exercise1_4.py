1095ceb86e19d796e781362991d1422a9eacf3cf
Fabian Frank <frankfabian945@outlook.de>
2025-09-15 13:56:28 +0300

    Update exercise1_4.py

--- exercises 1/exercise1_4.py
+++ exercises 1/exercise1_4.py
@@ -1,14 +1,14 @@
 #User input for the desired amount of minutes
-minutes_input=input("Enter number of minutes: ")
+minutes_input = input("Enter number of minutes: ")
 
 #try the script, if it fails(e.g. due to string in the input) return error message
 try:
     #convert input into int
-    minutes_input=int(minutes_input)
+    minutes_input = int(minutes_input)
 
     #divide minutes by 60 to get the hours, the remainder are the minutes
-    hours=minutes_input // 60
-    minutes=minutes_input % 60
+    hours = minutes_input // 60
+    minutes = minutes_input % 60
 
 
     #print the conversion of minutes into hours and minutes
