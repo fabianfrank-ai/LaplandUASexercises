cc974b980f2323d7df7780775f8a68daf3bf0775
Fabian Frank <frankfabian945@outlook.de>
2025-09-15 13:53:06 +0300

    Update exercise2_3.py

--- exercises 2/exercise2_3.py
+++ exercises 2/exercise2_3.py
@@ -1,19 +1,19 @@
 #get the salary and tax percentage by the user
-salary=input("Your monthly salary")
-tax_percentage=input("Your tax percentage in %")
+salary = input("Your monthly salary")
+tax_percentage = input("Your tax percentage in %")
 
 try:
     #convert strings into floats if possible, if not go into except bracket
-    salary=float(salary)
-    tax_percentage=float(tax_percentage)
+    salary = float(salary)
+    tax_percentage = float(tax_percentage)
 
     #calculate and round the tax percentage in %
-    taxes=salary*tax_percentage/100
-    taxes=round(taxes,2)
+    taxes = salary*tax_percentage/100
+    taxes = round(taxes,2)
 
     #deduct the taxes from the salary and round it
-    earnings=salary-taxes
-    earnings=round(earnings,2)
+    earnings = salary-taxes
+    earnings = round(earnings,2)
 
     #print it out to the user
     print(f'Earnings:{earnings}')
