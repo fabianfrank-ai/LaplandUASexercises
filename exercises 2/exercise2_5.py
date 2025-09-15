265a6a8925e62c9a94ee7a19a7cf925e86e334ea
Fabian Frank <frankfabian945@outlook.de>
2025-09-15 13:53:57 +0300

    Update exercise2_5.py

--- exercises 2/exercise2_5.py
+++ exercises 2/exercise2_5.py
@@ -2,14 +2,14 @@
 import random
 
 #create random int between 1 and 10 as random number
-random_number=random.randint(1,10)
+random_number = random.randint(1,10)
 
 #create random ints between 2 and 5 for the sides of the rectangle
-random_side1=random.randint(2,5)
-random_side2=random.randint(2,5)
+random_side1 = random.randint(2,5)
+random_side2 = random.randint(2,5)
 
 #calculate the area of the rectangle with the given values
-area_rectangle=random_side1*random_side2
+area_rectangle = random_side1*random_side2
 
 #print the values which were generated/calculated above
 print(f'Random Number: {random_number}')
